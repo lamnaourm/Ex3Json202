b853afd49510f29390323d54e2ccc887fcec2b7c
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-30 13:59:35 +0100

    + Methode de chargement de fichier JSON

--- app/src/main/java/com/example/ex3json202/MainActivity.java
+++ app/src/main/java/com/example/ex3json202/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String loadJsonFromRaw(int resId){
+        try {
+            InputStream in = getResources().openRawResource(resId);
+            byte[] data = new byte[in.available()];
+            in.read(data);
+            return new String(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
